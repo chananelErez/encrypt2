8d9ef50754302386878cf2f2205656e490355741
chananelErez <matanchananelliron@gmail.com>
2016-01-11 16:52:32 +0200

    changing the listener message if it is whole directory from the one the can received if it was single file.

--- src/main/java/observer/EncryptionLogEventArgs.java
+++ src/main/java/observer/EncryptionLogEventArgs.java
@@ -32,19 +32,32 @@ public class EncryptionLogEventArgs {
 		if(event.geteORd()=="Encryption"){
 			if(event.getAlgorithm()!=null){
 				databaseE.put(event.getFile(), event);
+				if(event.getFile().toLowerCase().endsWith(".txt")){
 				System.out.print("The Encryption of the file "+event.getFile()
 				                    +" was started.\n");
+				}else{
+					System.out.print("The Encryption of the folder "+event.getFile()
+                    +" was started.\n");
+				}
 			}else{
 				EncryptionEvent preEvent=databaseE.get(event.getFile());
 				long t=event.getCurrentTime()-preEvent.getCurrentTime();
 				String T=String.valueOf(t);
-				System.out.println("The encryption for file "+preEvent.getFile());
-				System.out.println("The encryption used the algorithm "
-				                    +preEvent.getAlgorithm());
-				System.out.println("The encryption took "+T+" milliseconds." );
-				System.out.println("The encrtpted file is located in file "+
-				                    preEvent.getOutputFile());
-				
+				if(event.getFile().toLowerCase().endsWith(".txt")){
+					System.out.println("The encryption for file "+preEvent.getFile());
+					System.out.println("The encryption used the algorithm "
+					                    +preEvent.getAlgorithm());
+					System.out.println("The encryption took "+T+" milliseconds." );
+					System.out.println("The encrtpted file is located in file "+
+					                    preEvent.getOutputFile());
+					}else{
+						System.out.println("The encryption of the folder "+preEvent.getFile());
+						System.out.println("The encryption used the algorithm "
+						                    +preEvent.getAlgorithm());
+						System.out.println("The whole encryption took "+T+" milliseconds." );
+						System.out.println("The encrtpted folder is located in "+
+						                    preEvent.getOutputFile());
+					}
 			}
 			
 			
@@ -53,16 +66,32 @@ public class EncryptionLogEventArgs {
 		if(event.geteORd()=="Decryption"){
 			if(event.getAlgorithm()!=null){
 				databaseD.put(event.getFile(), event);
+				if(event.getFile().toLowerCase().endsWith(".txt")){
+				System.out.print("The Decryption of the file "+event.getFile()
+				                    +" was started.\n");
+				}else{
+					System.out.print("The Decryption of the folder "+event.getFile()
+                    +" was started.\n");
+				}
 			}else{
 				EncryptionEvent preEvent=databaseD.get(event.getFile());
 				long t=event.getCurrentTime()-preEvent.getCurrentTime();
 				String T=String.valueOf(t);
-				System.out.println("The decryption for file "+preEvent.getFile());
-				System.out.println("The decryption used the algorithm "
-				                    +preEvent.getAlgorithm());
-				System.out.println("The decryption took "+T+" milliseconds." );
-				System.out.println("The decrtpted file is located in file "+
-				                    preEvent.getOutputFile());
+				if(event.getFile().toLowerCase().endsWith(".txt")){
+					System.out.println("The decryption for file "+preEvent.getFile());
+					System.out.println("The decryption used the algorithm "
+					                    +preEvent.getAlgorithm());
+					System.out.println("The decryption took "+T+" milliseconds." );
+					System.out.println("The decrtpted file is located in file "+
+					                    preEvent.getOutputFile());
+					}else{
+						System.out.println("The decryption of the folder "+preEvent.getFile());
+						System.out.println("The decryption used the algorithm "
+						                    +preEvent.getAlgorithm());
+						System.out.println("The whole decryption took "+T+" milliseconds." );
+						System.out.println("The decrtpted folder is located in "+
+						                    preEvent.getOutputFile());
+					}
 				
 			}
 			
